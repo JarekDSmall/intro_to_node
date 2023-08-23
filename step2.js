9567e328acbc35bd3a779dba6d82d7a837315847
JarekDSmall <129123790+JarekDSmall@users.noreply.github.com>
2023-08-22 21:09:05 -0400

    Add files via upload
    added step 2

---
+++ step2.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const axios = require('axios');
+
+function cat(path) {
+  fs.readFile(path, 'utf8', (err, data) => {
+    if (err) {
+      console.error(`Error reading ${path}:\n  ${err}`);
+      return;
+    }
+    console.log(data);
+  });
+}
+
+function webCat(url) {
+  axios.get(url)
+    .then(response => {
+      console.log(response.data);
+    })
+    .catch(error => {
+      console.error(`Error fetching ${url}:\n  ${error}`);
+    });
+}
+
+const arg = process.argv[2];
+if (arg.startsWith('http')) {
+  webCat(arg);
+} else {
+  cat(arg);
+}
